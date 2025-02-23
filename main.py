890451ca58a9345915f056f755e3171b468abc7a
Bartosz <bartosz.burdalski@gmail.com>
2025-02-23 04:03:59 +0100

    Refactor app

--- main.py
+++ main.py
@@ -4,23 +4,72 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+import os
 from variables import USERNAME, PASSWORD
 
-chrome_options = Options()
-chrome_options.add_argument("--disable-search-engine-choice-screen")
-service = Service('chromedriver-mac-arm64/chromedriver')
-driver = webdriver.Chrome(options=chrome_options, service=service)
+class WebAutomation:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-search-engine-choice-screen")
+        download_path = os.getcwd()
 
-driver.get("https://demoqa.com/login")
+        prefs = {'download.default_directory': download_path}
+        chrome_options.add_experimental_option('prefs', prefs)
 
-username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
-password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
-login_button = driver.find_element(By.ID, 'login')
+        service = Service('chromedriver-mac-arm64/chromedriver')
+        self.driver = webdriver.Chrome(options=chrome_options, service=service)
 
-username_field.send_keys(USERNAME)
-password_field.send_keys(PASSWORD)
-driver.execute_script("arguments[0].click();", login_button)
 
-input("Press Enter to close the browser")
-driver.close()
+    def login(self, username, password):
+        self.driver.get("https://demoqa.com/login")
+
+        username_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+        password_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+        login_button = self.driver.find_element(By.ID, 'login')
+
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        self.driver.execute_script("arguments[0].click();", login_button)
+
+    def fill_form(self, username, email, current_address, permanent_address):
+        elements = (WebDriverWait(self.driver, 10).
+                    until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div'))))
+        elements.click()
+
+        text_box = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+        text_box.click()
+
+        fullname_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+        email_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+        current_address_field = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, 'currentAddress')))
+        permanent_address_field = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+        submit_button = self.driver.find_element(By.ID, 'submit')
+
+        fullname_field.send_keys(username)
+        email_field.send_keys(email)
+        current_address_field.send_keys(current_address)
+        permanent_address_field.send_keys(permanent_address)
+        self.driver.execute_script("arguments[0].click();", submit_button)
+
+    def download(self):
+        upload_download = (WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-7'))))
+        upload_download.click()
+        download_button = self.driver.find_element(By.ID, 'downloadButton')
+        self.driver.execute_script("arguments[0].click();", download_button)
+
+    def close(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    web_automation = WebAutomation()
+    web_automation.login(USERNAME, PASSWORD)
+    web_automation.fill_form(USERNAME,
+                             "lorem@gmail.com",
+                             "Address 123, lorem ipsum",
+                             "Address 321, ipsum lorem")
+    web_automation.download()
+    web_automation.close()
+
